6cc86a8eb391d27e46b7e959cb4e7dbd8b1fc3ac
U-AFRILAND\osee_eboua <osee_eboua@DSINT-10071.AFRILAND.com>
2023-02-06 08:01:14 +0100

    project initialized

---
+++ src/test/java/com/afriland/soapservice/SoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afriland.soapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
